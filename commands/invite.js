d12089f3c8a8051317f016be367151dc1085c939
TheJeme <eemelijoonatan@gmail.com>
2021-06-23 11:14:17 +0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -8,7 +8,7 @@ module.exports = {
       .setAuthor("Foxel Invitation Link", bot.user.displayAvatarURL())
       .setColor(0xf66464)
       .setDescription(
-        "https://discord.com/api/oauth2/authorize?client_id=855042237687791646&permissions=8&scope=bot"
+        "https://discord.com/api/oauth2/authorize?client_id=855042237687791646&permissions=2048&scope=bot"
       );
     msg.channel.send(embed);
   },
