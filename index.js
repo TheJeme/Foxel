dc07dcac8a6844eca0e0a4541fd7b48cceb6ad54
Eemeli Mark <eemeli.mark@vertex.fi>
2024-12-23 18:31:23 +0200

    Fix

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 const Discord = require("discord.js");
-const bot = new Discord.Client();
+const { Intents } = require("discord.js");
+const bot = new Discord.Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.MESSAGE_CONTENT,
+  ],
+});
 bot.commands = new Discord.Collection();
 const botCommands = require("./commands");
 const prefix = ">";
@@ -17,7 +24,7 @@ bot.on("ready", async () => {
   bot.user.setActivity(`${prefix}help`);
 });
 
-bot.on("message", (msg) => {
+bot.on("messageCreate", (msg) => {
   const args = msg.content.split(/ +/);
   const command = args.shift().toLowerCase();
 
